2ae2895ed619a7eff704a354a3fd5e3673d3c62b
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-17 08:47:58 -0600

    Add background image, heading, and typing effect to sub-heading

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '../components/header/Header';
 import Navbar from '../components/navbar/Navbar';
 import './App.css';
 
 function App() {
-	return <Navbar />;
+	return (
+		<>
+			<Navbar />
+			<Header />
+		</>
+	);
 }
 
 export default App;
