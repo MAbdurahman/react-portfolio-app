c7bbb9c8bc156fe1053e6bec3c5e09b3d54a4ed3
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-18 17:13:18 -0600

    Modify: add more structure mark-up and styles for Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,44 +3,74 @@ import cityGuide from '../../img/portfolio/city-guide-app.png';
 import netflix from '../../img/portfolio/netflix.png';
 import portfolio from '../../img/portfolio/portfolio.png';
 import taskManager from '../../img/portfolio/task-manager.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearchPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function Portfolio() {
-   return (
+	return (
 		<div className='portfolio-wrapper'>
 			<div className='container'>
 				<h1 className='text-uppercase text-center py-5'>portfolio</h1>
 				<div className='image-box-wrapper row justify-content-center'>
-					<img
-						className='portfolio-image'
-						src={netflix}
-						alt='Netflix Clone Project'
-					/>
-				</div>
-				{/* - */}
-				<div className='image-box-wrapper row justify-content-center'>
-					<img
-						className='portfolio-image'
-						src={cityGuide}
-						alt='City Guide Project'
-					/>
-				</div>
-				{/* - */}
-				<div className='image-box-wrapper row justify-content-center'>
-					<img
-						className='portfolio-image'
-						src={portfolio}
-						alt='React and Material UI Project'
-					/>
-				</div>
-				{/* - */}
-				<div className='image-box-wrapper row justify-content-center'>
-					<img
-						className='portfolio-image'
-						src={taskManager}
-						alt='React and Redux Project'
-					/>
+					<div className='portfolio-image-box'>
+						<img
+							className='portfolio-image'
+							src={netflix}
+							alt='Netflix Clone Project'
+						/>
+						<div className='overflow'></div>
+						<FontAwesomeIcon
+							className='portfolio-icon'
+							icon={faSearchPlus}
+						/>
+					</div>
+
+					{/* - */}
+
+					<div className='portfolio-image-box'>
+						<img
+							className='portfolio-image'
+							src={cityGuide}
+							alt='City Guide Project'
+						/>
+						<div className='overflow'></div>
+						<FontAwesomeIcon
+							className='portfolio-icon'
+							icon={faSearchPlus}
+						/>
+					</div>
+
+					{/* - */}
+
+					<div className='portfolio-image-box'>
+						<img
+							className='portfolio-image'
+							src={portfolio}
+							alt='React and Material UI Project'
+						/>
+						<div className='overflow'></div>
+						<FontAwesomeIcon
+							className='portfolio-icon'
+							icon={faSearchPlus}
+						/>
+					</div>
+
+					{/* - */}
+
+					<div className='portfolio-image-box'>
+						<img
+							className='portfolio-image'
+							src={taskManager}
+							alt='React and Redux Project'
+						/>
+						<div className='overflow'></div>
+						<FontAwesomeIcon
+							className='portfolio-icon'
+							icon={faSearchPlus}
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
 	);
-};
+}
