b2ab30261a807cc7ab5658595931ed0df0cdcbc8
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-19 15:44:51 -0600

    Complete a portion of the mark-up structure for Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,9 +10,50 @@ export default function Contact() {
 					contact you as soon as possible.
 				</p>
 			</div>
-         <div className="container">
-            
-         </div>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-md-6 col-xs-12'>
+						<input
+							className='form-control'
+							type='text'
+							placeholder='First and last name'
+							name='name'
+						/>
+
+						<input
+							className='form-control'
+							type='text'
+							placeholder='Phone number'
+							name='phone'
+						/>
+
+						<input
+							className='form-control'
+							type='email'
+							placeholder='First and last name'
+							name='email'
+						/>
+
+						<input
+							className='form-control'
+							type='text'
+							placeholder='Subject'
+							name='subject'
+						/>
+					</div>
+					<div className='col-md-6 col-xs-12'>
+						<textarea
+							className='form-control'
+							type='text'
+							placeholder='Message'
+							name='description'
+						></textarea>
+						<button className='btn-main-offer contact-btn' type='submit'>
+							send message
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
