987d4cd12caacbda11c1f8e1551a19585ff17dc2
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-19 16:41:44 -0600

    Complete second portion of Contact.jsx and styles

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,48 +6,65 @@ export default function Contact() {
 			<div className='text-center'>
 				<h1>contact me</h1>
 				<p>
-					Fill out the form, describe your project needs, and I will
+					Fill out the form, briefly describe your project, and I will
 					contact you as soon as possible.
 				</p>
 			</div>
 			<div className='container'>
 				<div className='row'>
 					<div className='col-md-6 col-xs-12'>
-						<input
-							className='form-control'
-							type='text'
-							placeholder='First and last name'
-							name='name'
-						/>
+						<div className='text-center'>
+							<input
+								className='form-control'
+								type='text'
+								placeholder='First and last name'
+								name='name'
+							/>
+							<div className='line'></div>
+						</div>
 
-						<input
-							className='form-control'
-							type='text'
-							placeholder='Phone number'
-							name='phone'
-						/>
+						<div className='text-center'>
+							<input
+								className='form-control'
+								type='text'
+								placeholder='Phone number'
+								name='phone'
+							/>
+							<div className='line'></div>
+						</div>
 
-						<input
-							className='form-control'
-							type='email'
-							placeholder='First and last name'
-							name='email'
-						/>
+						<div className='text-center'>
+							<input
+								className='form-control'
+								type='email'
+								placeholder='Email'
+								name='email'
+							/>
+							<div className='line'></div>
+						</div>
 
-						<input
-							className='form-control'
-							type='text'
-							placeholder='Subject'
-							name='subject'
-						/>
+						<div className='text-center'>
+							<input
+								className='form-control'
+								type='text'
+								placeholder='Subject'
+								name='subject'
+							/>
+							<div className='line'></div>
+						</div>
 					</div>
+
 					<div className='col-md-6 col-xs-12'>
-						<textarea
-							className='form-control'
-							type='text'
-							placeholder='Message'
-							name='description'
-						></textarea>
+						<div className='text-center'>
+							<textarea
+								className='form-control'
+								type='text'
+								placeholder='Brief Message'
+								name='description'
+							></textarea>
+							<div className='line'></div>
+						</div>
+
 						<button className='btn-main-offer contact-btn' type='submit'>
 							send message
 						</button>
