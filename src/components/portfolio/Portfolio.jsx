6368e68a52f838ea2a82df4e1852ee319d271dfa
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-19 04:26:19 -0600

    Complete Portfolio Section with responsiveness

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,14 +5,247 @@ import portfolio from '../../img/portfolio/portfolio.png';
 import taskManager from '../../img/portfolio/task-manager.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearchPlus } from '@fortawesome/free-solid-svg-icons';
+import { PopupboxManager, PopupboxContainer } from 'react-popupbox';
+import 'react-popupbox/dist/react-popupbox.css';
 
 export default function Portfolio() {
+	//**************** Netflix ****************//
+	const openPopupboxNetflix = () => {
+		const content = (
+			<>
+				<img
+					className='portfolio-image-popupbox'
+					src={netflix}
+					alt='Netflix Clone Project'
+				/>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex
+					cumque illo est expedita quos adipisci suscipit unde itaque qui
+					perferendis.
+				</p>
+				<b>GitHub:</b>{' '}
+				<a
+					className='hyper-link'
+					onClick={() =>
+						window.open('https://github.com/8020Coding/netflix-project')
+					}
+				>
+					https://github.com/8020Coding/netflix-project
+				</a>
+			</>
+		);
+		PopupboxManager.open({ content });
+		PopupboxManager.update({
+			content,
+			config: {
+				titleBar: {
+					text: 'Netflix Clone Project'
+				}
+			}
+		});
+	};
+
+	const popupboxConfigNetflix = {
+		titleBar: {
+			enable: true,
+			text: 'Netflix Clone Project',
+		},
+		fadeIn: true,
+		fadeInSpeed: 500,
+	};
+
+	//**************** City Guide App ****************//
+	const openPopupboxCityGuide = () => {
+		const content = (
+			<>
+				<img
+					className='portfolio-image-popupbox'
+					src={cityGuide}
+					alt='City Guide App Project'
+				/>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex
+					cumque illo est expedita quos adipisci suscipit unde itaque qui
+					perferendis.
+				</p>
+				<b>Demo:</b>{' '}
+				<a
+					className='hyper-link'
+					onClick={() =>
+						window.open(
+							'https://city-guide-app-project.herokuapp.com/',
+							'_blank'
+						)
+					}
+				>
+					https://city-guide-app-project.herokuapp.com/
+				</a>
+				<br />
+				<b>GitHub:</b>{' '}
+				<a
+					className='hyper-link'
+					onClick={() =>
+						window.open(
+							'https://github.com/8020Coding/city-guide-app',
+							'_blank'
+						)
+					}
+				>
+					https://github.com/8020Coding/city-guide-app
+				</a>
+			</>
+		);
+		PopupboxManager.open({ content });
+		PopupboxManager.update({
+			content,
+			config: {
+				titleBar: {
+					text: 'City Guide App Project',
+				},
+			},
+		});
+	};
+
+	const popupboxConfigCityGuide = {
+		titleBar: {
+			enable: true,
+			text: 'City Guide App Project',
+		},
+		fadeIn: true,
+		fadeInSpeed: 500,
+	};
+
+	//**************** Portfolio Project ****************//
+	const openPopupboxPortfolio = () => {
+		const content = (
+			<>
+				<img
+					className='portfolio-image-popupbox'
+					src={portfolio}
+					alt='Portfolio Project'
+				/>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex
+					cumque illo est expedita quos adipisci suscipit unde itaque qui
+					perferendis.
+				</p>
+				<b>Demo:</b>{' '}
+				<a
+					className='hyper-link'
+					onClick={() =>
+						window.open(
+							'https://portfolio-rea-and-material-ui.herokuapp.com/',
+							'_blank'
+						)
+					}
+				>
+					https://portfolio-rea-and-material-ui.herokuapp.com/
+				</a>
+				<br />
+				<b>GitHub:</b>{' '}
+				<a
+					className='hyper-link'
+					onClick={() =>
+						window.open(
+							'https://github.com/8020Coding/portfilio',
+							'_blank'
+						)
+					}
+				>
+					https://github.com/8020Coding/portfilio
+				</a>
+			</>
+		);
+		PopupboxManager.open({ content });
+		PopupboxManager.update({
+			content,
+			config: {
+				titleBar: {
+					text: 'Portfolio Project with Material UI Library',
+				},
+			},
+		});
+	};
+
+	const popupboxConfigPortfolio = {
+		titleBar: {
+			enable: true,
+			text: 'Portfolio Project with Material UI Library',
+		},
+		fadeIn: true,
+		fadeInSpeed: 500,
+	};
+
+	//**************** Task Manager ****************//
+	const openPopupboxTaskManager = () => {
+		const content = (
+			<>
+				<img
+					className='portfolio-image-popupbox'
+					src={taskManager}
+					alt='Task Manager React and Redux Project'
+				/>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex
+					cumque illo est expedita quos adipisci suscipit unde itaque qui
+					perferendis.
+				</p>
+				<b>Demo:</b>{' '}
+				<a
+					className='hyper-link'
+					onClick={() =>
+						window.open(
+							'https://react-redux-task-manager.herokuapp.com/',
+							'_blank'
+						)
+					}
+				>
+					https://react-redux-task-manager.herokuapp.com/
+				</a>
+				<br />
+				<b>GitHub:</b>{' '}
+				<a
+					className='hyper-link'
+					onClick={() =>
+						window.open(
+							'https://github.com/8020Coding/task-manager',
+							'_blank'
+						)
+					}
+				>
+					https://github.com/8020Coding/task-manager
+				</a>
+			</>
+		);
+		PopupboxManager.open({ content });
+		PopupboxManager.update({
+			content,
+			config: {
+				titleBar: {
+					text: 'Task Manager with React and Redux',
+				},
+			},
+		});
+	};
+
+	const popupboxConfigTaskManager = {
+		titleBar: {
+			enable: true,
+			text: 'Task Manager with React and Redux',
+		},
+		fadeIn: true,
+		fadeInSpeed: 500,
+	};
+
 	return (
 		<div className='portfolio-wrapper'>
 			<div className='container'>
 				<h1 className='text-uppercase text-center py-5'>portfolio</h1>
 				<div className='image-box-wrapper row justify-content-center'>
-					<div className='portfolio-image-box'>
+					<div
+						className='portfolio-image-box'
+						onClick={openPopupboxNetflix}
+					>
 						<img
 							className='portfolio-image'
 							src={netflix}
@@ -27,7 +260,10 @@ export default function Portfolio() {
 
 					{/* - */}
 
-					<div className='portfolio-image-box'>
+					<div
+						className='portfolio-image-box'
+						onClick={openPopupboxCityGuide}
+					>
 						<img
 							className='portfolio-image'
 							src={cityGuide}
@@ -42,7 +278,10 @@ export default function Portfolio() {
 
 					{/* - */}
 
-					<div className='portfolio-image-box'>
+					<div
+						className='portfolio-image-box'
+						onClick={openPopupboxPortfolio}
+					>
 						<img
 							className='portfolio-image'
 							src={portfolio}
@@ -57,7 +296,10 @@ export default function Portfolio() {
 
 					{/* - */}
 
-					<div className='portfolio-image-box'>
+					<div
+						className='portfolio-image-box'
+						onClick={openPopupboxTaskManager}
+					>
 						<img
 							className='portfolio-image'
 							src={taskManager}
@@ -71,6 +313,10 @@ export default function Portfolio() {
 					</div>
 				</div>
 			</div>
+			<PopupboxContainer {...popupboxConfigNetflix} />
+			<PopupboxContainer {...popupboxConfigCityGuide} />
+			<PopupboxContainer {...popupboxConfigPortfolio} />
+			<PopupboxContainer {...popupboxConfigTaskManager} />
 		</div>
 	);
 }
