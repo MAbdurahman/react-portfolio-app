a3c2314dd632e97c88bd3bf5a83f8ab7de995ee0
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-19 17:38:08 -0600

    First part of adding functionality to Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
 export default function Contact() {
+
+	const serviceID = 'service_id';
+	const templateID = 'template_default';
+	const userID = 'user_YzQBSK3b0qdtzccL3lnIQ';
+
+function sendEmail(e) {
+	e.preventDefault();
+
+	emailjs.sendForm(serviceID, templateID, e.target, userID).then(
+		result => {
+			console.log(result.text);
+		},
+		error => {
+			console.log(error.text);
+		}
+	);
+}
+
    return (
 		<div id='contacts' className='contacts'>
 			<div className='text-center'>
@@ -11,65 +30,70 @@ export default function Contact() {
 				</p>
 			</div>
 			<div className='container'>
-				<div className='row'>
-					<div className='col-md-6 col-xs-12'>
-						<div className='text-center'>
-							<input
-								className='form-control'
-								type='text'
-								placeholder='First and last name'
-								name='name'
-							/>
-							<div className='line'></div>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className='row'>
+						<div className='col-md-6 col-xs-12'>
+							<div className='text-center'>
+								<input
+									className='form-control'
+									type='text'
+									placeholder='First and last name'
+									name='name'
+								/>
+								<div className='line'></div>
+							</div>
+
+							<div className='text-center'>
+								<input
+									className='form-control'
+									type='text'
+									placeholder='Phone number'
+									name='phone'
+								/>
+								<div className='line'></div>
+							</div>
+
+							<div className='text-center'>
+								<input
+									className='form-control'
+									type='email'
+									placeholder='Email'
+									name='email'
+								/>
+								<div className='line'></div>
+							</div>
+
+							<div className='text-center'>
+								<input
+									className='form-control'
+									type='text'
+									placeholder='Subject'
+									name='subject'
+								/>
+								<div className='line'></div>
+							</div>
 						</div>
 
-						<div className='text-center'>
-							<input
-								className='form-control'
-								type='text'
-								placeholder='Phone number'
-								name='phone'
-							/>
-							<div className='line'></div>
-						</div>
+						<div className='col-md-6 col-xs-12'>
+							<div className='text-center'>
+								<textarea
+									className='form-control'
+									type='text'
+									placeholder='Brief Message'
+									name='description'
+								></textarea>
+								<div className='line'></div>
+							</div>
 
-						<div className='text-center'>
-							<input
-								className='form-control'
-								type='email'
-								placeholder='Email'
-								name='email'
-							/>
-							<div className='line'></div>
-						</div>
-
-						<div className='text-center'>
-							<input
-								className='form-control'
-								type='text'
-								placeholder='Subject'
-								name='subject'
-							/>
-							<div className='line'></div>
+							<button
+								className='btn-main-offer contact-btn'
+								type='submit'
+							>
+								send message
+							</button>
 						</div>
 					</div>
-
-					<div className='col-md-6 col-xs-12'>
-						<div className='text-center'>
-							<textarea
-								className='form-control'
-								type='text'
-								placeholder='Brief Message'
-								name='description'
-							></textarea>
-							<div className='line'></div>
-						</div>
-
-						<button className='btn-main-offer contact-btn' type='submit'>
-							send message
-						</button>
-					</div>
-				</div>
+				</form>
 			</div>
 		</div>
 	);
