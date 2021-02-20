53d4b44fea5489dbf7fd9ef84d81173090f52db3
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-19 19:29:42 -0600

    Complete Contact Section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
+import { useForm } from 'react-hook-form';
 
 export default function Contact() {
+	const [successMessage, setSuccessMessage] = useState('');
+	const { register, handleSubmit, errors } = useForm();
 
 	const serviceID = 'service_id';
 	const templateID = 'template_default';
 	const userID = 'user_YzQBSK3b0qdtzccL3lnIQ';
 
-function sendEmail(e) {
-	e.preventDefault();
+	const onSubmit = (data, r) => {
+		sendEmail(
+			serviceID,
+			templateID,
+			{
+				name: data.name,
+				phone: data.phone,
+				email: data.email,
+				subject: data.subject,
+				description: data.description,
+			},
+			userID
+		)
+		r.target.reset();
 
-	emailjs.sendForm(serviceID, templateID, e.target, userID).then(
-		result => {
-			console.log(result.text);
-		},
-		error => {
-			console.log(error.text);
-		}
-	);
-}
+	}
+
+
+const sendEmail = (serviceID, templateID, variables, userID) => {
+
+	emailjs
+		.send(serviceID, templateID, variables, userID)
+		.then(() => {
+			setSuccessMessage(
+				"Your message was successfully sent!  I'll contact you as soon as possible."
+			);
+		})
+		.catch(err => console.error(`Something went wrong ${err}`));
+};
 
    return (
 		<div id='contacts' className='contacts'>
@@ -28,6 +48,7 @@ function sendEmail(e) {
 					Fill out the form, briefly describe your project, and I will
 					contact you as soon as possible.
 				</p>
+				<span className='success-message'>{successMessage}</span>
 			</div>
 			<div className='container'>
 				<form onSubmit={handleSubmit(onSubmit)}>
@@ -39,9 +60,19 @@ function sendEmail(e) {
 									type='text'
 									placeholder='First and last name'
 									name='name'
+									ref={register({
+										required: 'First and last name are required!',
+										pattern: {
+											value: /^([a-zA-Z]{2,}\s[a-zA-z]{1,}'?-?[a-zA-Z]{1,}\s?([a-zA-Z]{1,})?)/i,
+											message: 'Invalid First and Last Name!',
+										},
+									})}
 								/>
 								<div className='line'></div>
 							</div>
+							<span className='error-message'>
+								{errors.name && errors.name.message}
+							</span>
 
 							<div className='text-center'>
 								<input
@@ -49,9 +80,19 @@ function sendEmail(e) {
 									type='text'
 									placeholder='Phone number'
 									name='phone'
+									ref={register({
+										required: 'Phone number is required!',
+										pattern: {
+											value: /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s.]{0,1}[0-9]{3}[-\s.]{0,1}[0-9]{4}$/i,
+											message: 'Invalid Phone Number!',
+										},
+									})}
 								/>
 								<div className='line'></div>
 							</div>
+							<span className='error-message'>
+								{errors.phone && errors.phone.message}
+							</span>
 
 							<div className='text-center'>
 								<input
@@ -59,9 +100,19 @@ function sendEmail(e) {
 									type='email'
 									placeholder='Email'
 									name='email'
+									ref={register({
+										required: 'Email address is required!',
+										pattern: {
+											value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+											message: 'Invalid Email Address!',
+										},
+									})}
 								/>
 								<div className='line'></div>
 							</div>
+							<span className='error-message'>
+								{errors.email && errors.email.message}
+							</span>
 
 							<div className='text-center'>
 								<input
@@ -69,9 +120,15 @@ function sendEmail(e) {
 									type='text'
 									placeholder='Subject'
 									name='subject'
+									ref={register({
+										required: 'A Subject is required!',
+									})}
 								/>
 								<div className='line'></div>
 							</div>
+							<span className='error-message'>
+								{errors.subject && errors.subject.message}
+							</span>
 						</div>
 
 						<div className='col-md-6 col-xs-12'>
@@ -81,10 +138,20 @@ function sendEmail(e) {
 									type='text'
 									placeholder='Brief Message'
 									name='description'
+									ref={register({
+										required: 'A Brief Message is required!',
+										minLength: {
+											value: 30,
+											message:
+												'Your message must be at least 30 characters!',
+										},
+									})}
 								></textarea>
 								<div className='line'></div>
 							</div>
-
+							<span className='error-message'>
+								{errors.description && errors.description.message}
+							</span>
 							<button
 								className='btn-main-offer contact-btn'
 								type='submit'
