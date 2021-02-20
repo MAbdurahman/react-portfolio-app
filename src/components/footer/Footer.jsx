5d61a3493b6f2b9ad09181073d64991cbbefa51c
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-19 19:43:28 -0600

    Start of Footer.jsx

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Footer() {
+   return (
+      <div className='footer'>
+         <div className="container">
+            <div className="row">
+               <div className="col-lg-4 col-md-6 col-sm-6">
+                  <div className="d-flex">
+                     
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+};
